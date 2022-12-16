f4ab9ca5eda67febd22c4312a549b8214ec73167
leoneville <leoneville_@hotmail.com>
2022-12-15 23:03:19 -0300

    test: criando testes unitarios e de integracao para user

---
+++ utils/random.go
@@ -0,0 +1,24 @@
+package utils
+
+import (
+	"fmt"
+	"math/rand"
+	"strings"
+)
+
+const alphabet string = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+func RandomString(number int) string {
+	var sb strings.Builder
+	k := len(alphabet)
+	for i := 0; i < number; i++ {
+		c := alphabet[rand.Intn(k)]
+		sb.WriteByte(c)
+	}
+
+	return sb.String()
+}
+
+func RandomEmail(number int) string {
+	return fmt.Sprintf("%s@email.com", RandomString(number))
+}
